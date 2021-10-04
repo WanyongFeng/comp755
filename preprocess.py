6883b57408c6815404d544bc65fd6370d5aff9f9
WanyongFeng <46601448+WanyongFeng@users.noreply.github.com>
2021-10-03 22:13:08 -0400

    fdsfs

---
+++ preprocess.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  3 22:11:22 2021
+
+@author: wanyongfeng
+"""
+
+import pandas as pd
+import numpy as np
+import re
+
+true = pd.read_csv("True.csv")
+# false = pd.read_csv("Fake.csv")
+
+textst = list(true.loc[:,"text"])
+# textsf = list(false.loc[:,"text"])
+
+# texts = np.array(textst + textsf)
+texts = np.array(textst)
+
+words = set()
+punc = '''!()-[]{};:'"\,<>./?@#%^&*_~'''
+for idx, text in enumerate(texts):
+    for ele in text:
+        if ele in punc:
+            text = text.replace(ele, "")
+    text = re.sub(' +', ' ', text)
+    texts[idx] = text    
+    temp = text.split()
+    words = words.union(set(temp))
+print(texts[0])
+
+words = list(words)
+element = dict.fromkeys(words, 0)
+elements = []
+
+for idx, text in enumerate(texts):
+    element = dict.fromkeys(words, 0)
+    temp = text.split()
+    for j in temp:
+        element[j] = element[j] + 1
+    elements.append(element)
+print(elements[0])
